f1301f03622ac82aeb761cd6ed17ab8dff22c10c
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-06-06 13:57:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -564,8 +564,8 @@ with tab6:
      drift_pct = ((curr_avg - ref_avg) / ref_avg) * 100
      st.metric("Traffic Drift %",f"{drift_pct:.2f}%",delta=f"{drift_pct:.2f}%")
      airport_dist = (df.groupby(['MONTH','APT_NAME'])['FLT_TOT_1'].sum().reset_index())
-     fig = px.scatter(airport_dist,x='MONTH',y='FLT_TOT_1',color='APT_NAME',title="Airport Traffic Distribution Drift", color_continuous_scale=[[0.0, "rgba(167, 243, 208, 0.3)"],[0.5, "rgba(52,211,153,0.6)"],[1.0, "rgba(5,150,105,0.9)"]])
-     fig.update_traces(marker=dict(size=16, line=dict(width=0)))
+     fig = px.scatter(airport_dist,x='MONTH',y='FLT_TOT_1',color='APT_NAME',title="Airport Traffic Distribution Drift", color_continuous_scale="Turbo")
+     fig.update_traces(marker=dict(opacity=0.65, line=dict(width=2,color="rgba(255,255,255,0.4)")))
      fig.update_layout(template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)",plot_bgcolor="rgba(0,0,0,0)")
      st.plotly_chart(fig, use_container_width=True)
      state_drift = (df.groupby('STATE_NAME')['FLT_TOT_1'].mean().sort_values(ascending=False).head(15))
@@ -578,8 +578,8 @@ with tab6:
      ifr_drift = curr_ifr - ref_ifr
      st.metric("IFR Ratio Drift",f"{curr_ifr:.2%}",delta=f"{(curr_ifr-ref_ifr):.2%}")
      ifr_trend= (df.groupby("MONTH")["IFR_RATIO"].mean().reset_index())
-     fig = px.scatter(ifr_trend,x='MONTH',y='IFR_RATIO',color='MONTH',title="Growth by Month",color_continuous_scale=[[0.0, "rgba(167, 243, 208, 0.3)"],[0.5, "rgba(52,211,153,0.6)"],[1.0, "rgba(5,150,105,0.9)"]])
-     fig.update_traces(marker=dict(size=16, line=dict(width=0)))
+     fig = px.scatter(ifr_trend,x='MONTH',y='IFR_RATIO',color='MONTH',title="Growth by Month",color_continuous_scale="Turbo)
+     fig.update_traces(marker=dict(opacity=0.65, line=dict(width=2,color="rgba(255,255,255,0.4)")))
      fig.update_layout(template="plotly_dark",paper_bgcolor="rgba(0,0,0,0)",plot_bgcolor="rgba(0,0,0,0)")
      st.plotly_chart(fig, use_container_width=True)       
      if abs(drift_pct) < 5:
