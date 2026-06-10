707eadb57e405e1df8b657413b0f5b11d2c51408
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-06-10 13:27:22 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -102,10 +102,10 @@ model.fit(X_train, y_train)
 # ?? EVALUATE
 # ===============================
 y_pred = model.predict(X_test)
-
+mae=mean_absolute_error(y_test, y_pred)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 r2 = r2_score(y_test, y_pred)
-
+print("MAE:", mae)
 print("RMSE:", rmse)
 print("R2:", r2)
 
